3e3c3ac7c2a7a4688bb9417c4044828a4f464f7c
hamdihunturk <hunturkh@gmail.com>
2024-12-08 02:23:37 +0300

    module common.

--- lib/feature/view/home_view.dart
+++ lib/feature/view/home_view.dart
@@ -1,6 +1,8 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:gen/gen.dart';
 import 'package:teamup/product/navigation/app_router.dart';
+import 'package:teamup/product/widget/project_network_image.dart';
 
 @RoutePage()
 class HomeView extends StatelessWidget {
@@ -8,14 +10,22 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      child: const Text("next_Page"),
-      onPressed: () {
-        // For heavy transitions like complex operations
-        context.router.push<bool>(HomeDetailRoute(id: "1"));
-        // For simple page navigation
-        context.pushRoute(HomeDetailRoute(id: "1"));
-      },
+    const url =
+        'https://www.kolaydi.com/static/media/profile.cfa978abb77702a3cd88.png';
+    return Column(
+      children: [
+        ProjectAssets.images.imgShip.image(),
+        const ProjectNetworkImage(url: url),
+        ElevatedButton(
+          child: const Text("next_Page"),
+          onPressed: () {
+            // For heavy transitions like complex operations
+            context.router.push<bool>(HomeDetailRoute(id: "1"));
+            // For simple page navigation
+            context.pushRoute(HomeDetailRoute(id: "1"));
+          },
+        ),
+      ],
     );
   }
 }
---
+++ module/common/lib/src/package/custom_image/custom_mem_cahe.dart
@@ -0,0 +1,7 @@
+/// Custom mem cache for using network image
+final class CustomMemCache {
+  const CustomMemCache({required this.height, required this.width});
+
+  final int height;
+  final int width;
+}
